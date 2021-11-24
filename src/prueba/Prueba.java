6f52daec50330f60e788e16978e0690028ff8ecd
ariana3912 <94937737+ariana3912@users.noreply.github.com>
2021-11-24 14:52:55 -0300

    Update Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -16,7 +16,7 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        int a=0;
         System.out.println("Rama Desarrollador");
     }
     
